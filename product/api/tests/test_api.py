009c0f742390c0fbf454af920ca1ac7d57e606f2
Артем Бахмутенко <kadomodify@gmail.com>
2024-08-20 18:06:14 +0400

    fix: user_products test

--- product/api/tests/test_api.py
+++ product/api/tests/test_api.py
@@ -7,6 +7,7 @@ from users.models import CustomUser
 from products.models import Product
 from products.lessons.models import Lesson
 from django.test import Client
+from users.user_products.models import UserProducts
 
 
 class ProductAPITests(APITestCase):
@@ -22,18 +23,42 @@ class ProductAPITests(APITestCase):
 		print(self.user.is_active)
 		self.client = Client()
 
-		self.lesson = Lesson.objects.create(
-			name='Тестовый урок',
-			video_url ='https://github.com/zow1e2k'
+		self.user_products = UserProducts.objects.create(
+			user=self.user,
 		)
 
-		self.product = Product.objects.create(
-			name='Тестовый продукт',
+		self.lessons = []
+		self.lessons.append(Lesson.objects.create(
+			name='Тестовый урок 1',
+			video_url ='https://github.com/zow1e2k'
+		))
+		self.lessons.append(Lesson.objects.create(
+			name='Тестовый урок 2',
+			video_url='https://github.com/zow1e2k'
+		))
+
+		self.products = []
+		self.products.append(Product.objects.create(
+			name='Тестовый продукт 1',
 			date_created=timezone.now(),
 			price=1000,
 			creator=self.user,
-			lesson=self.lesson
-		)
+			lesson=self.lessons[0]
+		))
+
+		self.products.append(Product.objects.create(
+			name='Тестовый продукт 2',
+			date_created=timezone.now(),
+			price=1000,
+			creator=self.user,
+			lesson=self.lessons[1]
+		))
+
+		for product in self.products:
+			if product.name == "Тестовый продукт 2":
+				continue
+
+			self.user_products.products.add(product)
 
 	def test_available_products(self):
 		self.client = Client()
